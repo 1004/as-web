b389121d80f7d823329985d6f774b761368f5067
xiekongying001 <xiekongying001@ke.com>
2021-02-25 10:49:52 +0800

    feature: 还原配置

--- src/page/config/add/config_add.js
+++ src/page/config/add/config_add.js
@@ -1,33 +1,72 @@
 import React from 'react'
 import './config_add.less'
 import API from '../../../service/api'
-import {Input, Button, message} from "antd";
+import {Input, Button, message, Popconfirm, Table} from "antd";
+import {configList} from "../../../service/config/api_config";
+
 
 const {TextArea} = Input;
 
+const PAGESIZE = 5;
 export default class ConfigAdd extends React.Component {
-    constructor(pros){
+    constructor(pros) {
         super(pros);
-        const {location:{item:{namespance}={}}={}} = this.props;
+        const {location: {item: {namespance, version, originalUrl} = {}} = {}} = this.props;
         this.namespace = namespance;
+        this.version = version;
+        this.originalUrl = originalUrl;
+        this.listDisplay = !!namespance;
+        this.columns = [
+            {
+                title: 'version',
+                dataIndex: 'version',
+            },
+            {
+                title: 'createTime',
+                dataIndex: 'createTime'
+            },
+            {
+                title: 'operation',
+                dataIndex: 'operation',
+                render: (text, record) => {
+                    const style = record.version === this.version ? {color: 'gray'} : null;
+                    return <Popconfirm
+                        title={`确定要还原到${record.version}吗？`}
+                        disabled={record.version === this.version}
+                        onConfirm={() => this.resetConifg(record)}
+                    >
+                        <a style={style}>还原</a>
+                    </Popconfirm>
+                }
+            }
+
+        ];
+        this.loadConfigList();
+        this.loadConfigText(originalUrl);
     }
+
     state = {
         configText: '',
-        submit: false
-    }
+        submit: false,
+        list:[],
+    };
 
     submit = () => {
         const {configText, submit} = this.state;
-        if (!this.namespace){
+        if (!this.namespace) {
             message.error("空间不能为空")
             return;
         }
         API.addConfig({name_space: this.namespace, content: configText})
             .then(res => res.json())
             .then(result => {
-                const {code, msg} = result;
+                const {code, msg,data:{}} = result;
                 if (code === 0) {
                     message.success("添加成功");
+                    this.loadConfigList();
+                    this.setState({
+                       submit:false
+                    });
                 } else {
                     message.success("添加失败：" + msg);
                 }
@@ -39,7 +78,7 @@ export default class ConfigAdd extends React.Component {
     };
 
     render() {
-        const {configText, submit} = this.state;
+        const {configText, submit,list} = this.state;
 
         return <div className="addConfig">
             <div className="addConfig-left">
@@ -61,7 +100,8 @@ export default class ConfigAdd extends React.Component {
                         className="addConfig-input"
                         onChange={({target: {value}}) => {
                             this.setState({
-                                configText: value
+                                configText: value,
+                                submit:this.configText !== value
                             })
                         }}
                         value={configText}
@@ -72,11 +112,69 @@ export default class ConfigAdd extends React.Component {
                 <div className='addConfig-btn'>
                     <Button type='primary'
                             onClick={this.submit}
+                            disabled={!submit}
                     >
                         提交发布
                     </Button>
                 </div>
             </div>
+            {
+                this.listDisplay ?
+                    <div className='addConfig-right'>
+                        <Table
+                            columns={this.columns}
+                            rowKey={item => item.id}
+                            pagination={false}
+                            size='small'
+                            footer={() => (<div> 支持还原最近的{PAGESIZE}条</div>)}
+                            dataSource={list}
+                        />
+                    </div>
+                    : null
+            }
+
         </div>
     }
+
+    resetConifg(record) {
+        this.loadConfigText(record.originalUrl);
+    }
+
+    loadConfigList = ()=>{
+        if (!this.namespace){
+            return;
+        }
+        API.configList({name_space:this.namespace,page_index:1,page_size:PAGESIZE})
+            .then(res=>res.json())
+            .then(result=>{
+                const {code,msg,data:{list,total}={}} = result;
+                this.setState({
+                    list
+                })
+            }).catch(e=>{
+                console.log(e);
+        })
+    }
+
+    loadConfigText=(originUrl)=>{
+        if (!originUrl) {
+            return;
+        }
+        fetch(originUrl)
+            .then(res=>res.text())
+            .then(result=>{
+                if (this.configText == null){
+                    this.configText = result;
+                }
+                const submit = this.configText !== result;
+
+                this.setState({
+                    configText:result,
+                    submit:submit
+                })
+            }).catch(e=>{
+                console.log(e);
+        })
+    }
+
 }
--- src/service/api_path.js
+++ src/service/api_path.js
@@ -1,4 +1,4 @@
-export const baseUrl = "http://127.0.0.1:5088";
+export const baseUrl = "http://127.0.0.1:8080/as";
 export const api = {
     categoryList: "/goods/category/query",
     addCategory: "/goods/category/add",
